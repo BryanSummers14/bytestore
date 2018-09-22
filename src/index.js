2616ca7bc4b78e978b2b0f2e40f1d112b763d77f
bryanshrugged <bsummerscon@merkleinc.com>
2018-09-21 18:24:07 -0600

    Initial Commit

---
+++ src/index.js
@@ -0,0 +1,94 @@
+import { assign } from './util';
+
+/**
+ * Creates a new store, which is a tiny evented state container.
+ * @name createStore
+ * @param {Object} [state={}]		Optional initial state
+ * @returns {store}
+ * @example
+ * let store = createStore();
+ * store.subscribe( state => console.log(state) );
+ * store.on('a', stateChange => console.log(stateChange))
+ * store.setState({ a: 'b' });   // logs { a: 'b'}
+ * store.setState({ c: 'd' });   // logs { a: 'b', c: 'd'}
+ * store.setState({ a: 'foo' }); // logs { a: 'foo', c: 'd' }, { a: 'foo' }
+ */
+export default function createStore(state) {
+	let listeners = [];
+	let onListeners = new Map();
+	state = state || {};
+	noop = () => {};
+
+	function setState(update, overwrite) {
+		state = overwrite ? update : assign(assign({}, state), update);
+		let currentListeners = listeners;
+		// Update all state subscribers first
+		for (let i=0; i<currentListeners.length; i++) {
+			currentListeners[i]._callback(state);
+			currentListeners[i]._effect(state);
+		}
+		// Update all listeners to 'slices' of state last
+		let stateChanged = Object.keys(update);
+		for (let j=0; j<stateChanged.length; j++) {
+		  let _key = stateChanged[j];
+		  if (onListeners.has(_key)) {
+			let listeners = onListeners.get(_key);
+			for (let k=0; k<listeners.length; k++) {
+			  listeners[k]._callback(state[_key])
+			  listeners[k]._effect(state[_key])
+			}
+		  }
+		}
+	}
+
+	/**
+	 * Attaches a listener with a callback to a slice of state
+	 * @param {String} stateSlice
+	 * @param {Function} callback
+	 */
+	function on(stateSlice, callback, effect = noop) {
+		if (!Boolean(onListeners.get(stateSlice))) onListeners.set(stateSlice, [])
+
+		const _call = { _callback: callback, _effect: effect }
+		onListeners.get(stateSlice).push(_call)
+	}
+
+	/**
+	 * An observable state container, returned from {@link createStore}
+	 * @name store
+	 */
+
+	return /** @lends store */ {
+
+		/**
+		 * Apply a partial state object to the current state, invoking registered listeners.
+		 * @param {Object} update				An object with properties to be merged into state
+		 * @param {Boolean} [overwrite=false]	If `true`, update will replace state instead of being merged into it
+		 */
+		setState,
+
+		/**
+		 * Attach a listener with a callback to a particular 'slice' of state
+		 * @param {String} stateSlice
+		 * @param {Function} callback  callback function to be fired when listener is detected
+		 */
+		on,
+
+		/**
+		 * Register a listener function to be called whenever state is changed function.
+		 * @param {Function} listener	A function to call when state changes. Gets passed the new state.
+		 */
+		subscribe(listener, effect = noop) {
+			const _listener = { _callback: listener, _effect: effect };
+			listeners.push(_listener);
+		},
+
+		/**
+		 * Retrieve the current state object.
+		 * @returns {Object} state
+		 */
+		getState() {
+			return state;
+		}
+	};
+}
