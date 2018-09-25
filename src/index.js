617786aeae7c64d82e52930456cc3c99cb1e36be
bryanshrugged <bsummerscon@merkleinc.com>
2018-09-25 07:50:14 -0600

    Updates to store

--- src/index.js
+++ src/index.js
@@ -17,15 +17,15 @@ export default function createStore(state) {
 	let listeners = [];
 	let onListeners = new Map();
 	state = state || {};
-	noop = () => {};
+	const noop = () => {};
 
 	function setState(update, overwrite) {
 		state = overwrite ? update : assign(assign({}, state), update);
 		let currentListeners = listeners;
 		// Update all state subscribers first
 		for (let i=0; i<currentListeners.length; i++) {
-			currentListeners[i]._callback(state);
-			currentListeners[i]._effect(state);
+			currentListeners[i]._li(state);
+			currentListeners[i]._ef(state);
 		}
 		// Update all listeners to 'slices' of state last
 		let stateChanged = Object.keys(update);
@@ -34,8 +34,8 @@ export default function createStore(state) {
 		  if (onListeners.has(_key)) {
 			let listeners = onListeners.get(_key);
 			for (let k=0; k<listeners.length; k++) {
-			  listeners[k]._callback(state[_key])
-			  listeners[k]._effect(state[_key])
+			  listeners[k]._li(state[_key])
+			  listeners[k]._ef(state[_key])
 			}
 		  }
 		}
@@ -49,7 +49,7 @@ export default function createStore(state) {
 	function on(stateSlice, callback, effect = noop) {
 		if (!Boolean(onListeners.get(stateSlice))) onListeners.set(stateSlice, [])
 
-		const _call = { _callback: callback, _effect: effect }
+		const _call = { _li: callback, _ef: effect }
 		onListeners.get(stateSlice).push(_call)
 	}
 
@@ -71,15 +71,17 @@ export default function createStore(state) {
 		 * Attach a listener with a callback to a particular 'slice' of state
 		 * @param {String} stateSlice
 		 * @param {Function} callback  callback function to be fired when listener is detected
+		 * @param {Function} effect  function to handle any side effects allowing callback to be a pure function
 		 */
 		on,
 
 		/**
 		 * Register a listener function to be called whenever state is changed function.
 		 * @param {Function} listener	A function to call when state changes. Gets passed the new state.
+		 * @param {Function} effect  function to handle any side effects allowing listener to be a pure function
 		 */
 		subscribe(listener, effect = noop) {
-			const _listener = { _callback: listener, _effect: effect };
+			const _listener = { _li: listener, _ef: effect };
 			listeners.push(_listener);
 		},
 
